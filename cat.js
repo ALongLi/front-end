620598e2620aa324ef977ddd62f1e8103f8252fe
“lal <lal@xdja.com>
2020-01-07 08:59:04 +0800

    fix(unuse): remove unuse code

--- cat.js
+++
@@ -1,9 +0,0 @@
-var childProcess = require('child_process');
-
-var ls = childProcess.exec('cat.js | wc', function (error, stdout, stderr) {
-   if (error) {
-     console.log(error.stack);
-     console.log('Error code: '+error.code);
-   }
-   console.log('Child Process STDOUT: '+stdout);
-});
